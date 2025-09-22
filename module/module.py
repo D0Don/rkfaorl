aebf21157826a5e0946e254839c12d183cea851a
D0Don <kimdongeon11@gmail.com>
2025-09-22 21:40:16 +0900

    module.py and gate'

--- module/module.py
+++ module/module.py
@@ -1,19 +1,33 @@
 andset = [(0,0,0),(0,1,0),(1,0,0),(1,1,1)]
 orset = [(0,0,0),(0,1,1),(1,0,1),(1,1,1)]
 
+
+
+
 class myandgate:
     def __init__(self):
         self.w1 = 0.5
         self.w2 = 0.5
         self.b = 0.5
+        self.lr = 0.001
     def annd(self,x1,x2):
         return (x1*self.w1+x2*self.w2+self.b)
+    def backward(self,x1,x2,y, yhat):
+        self.b = self.b+2*self.lr*(y-yhat)
+        self.w1 = self.w1+2*self.lr*x1*(y-yhat)
+        self.w2 = self.w2+2*self.lr*x2*(y-yhat)
+    
+            
+def getloss(y,yhat):
+    return (y - yhat)**2
         
 andgate = myandgate()
-
-
-def getloss(y,yhat):
-    return (y-yhat)**2
-for i in range(4):
-    print(getloss(andset[i][2],andgate.annd(andset[i][0],andset[i][0])))
-
+epoch = 1000
+for i in range(epoch):
+    loss=0
+    for x1, x2, y in andset:
+        y_pred = andgate.annd(x1, x2)
+        andgate.backward(x1,x2,y,y_pred)
+        print(y, y_pred)
+        loss+=getloss(y,y_pred)
+        print("loss:", loss/4)
